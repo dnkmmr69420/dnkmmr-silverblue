8e52d263d06c61637aaa98b17f89b19b9d2afe98
dnkmmr <104856296+dnkmmr69420@users.noreply.github.com>
2023-05-15 13:43:02 -0500

    Update autostart.sh

--- usr/share/ublue-os/firstboot/launcher/autostart.sh
+++ usr/share/ublue-os/firstboot/launcher/autostart.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
-# Simply launches the "yafti" GUI with the uBlue image's configuration.
-/usr/bin/yafti /usr/share/ublue-os/firstboot/yafti.yml
+[ -d /nix/store ] && echo “nix is already installed. skipping nix installation” || bash /usr/share/ublue-os/firstboot/nix-install.sh
+
+[ -f /etc/firstboot-check-dir/nix-apps ] && echo “nix has already installed its packages” || bash /usr/share/ublue-os/firstboot/nix-setup.sh
+
+[ -f /etc/firstboot-check-dir/flatpak-apps ] && echo “flatpaks have been already set up” || bash /usr/share/ublue-os/firstboot/nix-setup.sh
