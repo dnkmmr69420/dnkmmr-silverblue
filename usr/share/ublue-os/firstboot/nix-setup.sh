4a6d3483c2f4e3be919e27b983d5672dabbc6efe
dnkmmr <104856296+dnkmmr69420@users.noreply.github.com>
2023-05-16 15:43:35 -0500

    Update nix-setup.sh

--- usr/share/ublue-os/firstboot/nix-setup.sh
+++ usr/share/ublue-os/firstboot/nix-setup.sh
@@ -3,6 +3,8 @@ sudo nix profile install nixpkgs#distrobox
 sudo nix profile install nixpkgs#toolbox
 sudo nix profile install nixpkgs#htop
 sudo nix profile install nixpkgs#mc
+sudo nix profile install nixpkgs#trash-cli
+sudo nix profile install nixpkgs#noto-fonts
 
 sudo mkdir -p /etc/firstboot-check-dir
 sudo touch /etc/firstboot-check-dir/nix-apps
